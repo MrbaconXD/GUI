9c45606b16d83a30b0d02a3a0225c2cdd920a5a0
BaconXD <97074586+MrbaconXD@users.noreply.github.com>
2022-08-05 16:42:32 +0700

    Update ExecGUI.lua
    Execution the GUI.lua file

--- ExecGUI.lua
+++ ExecGUI.lua
@@ -3,8 +3,14 @@ local button = Instance.new('TextButton', gui)
 
 button.Size = UDim2.new(0, 200, 0, 100)
 button.Position = UDim2.new(.5, 0, .5, 0)
-button.Text = 'Press to access the GUI'
+button.Text = 'Press to access the GUI :D'
+
+local gui = Instance.new('ScreenGui', game:service'CoreGui')
+local button = Instance.new('TextButton', gui)
+
+button.Size = UDim2.new(0, 0, 0, 0)
+button.Position = UDim2.new(.6, 0, .0, 0)
+button.Text = '108 101 097 118 101 032 116 104 101 032 103 097 109 101 032 098 101 102 111 114 101 032 105 116 032 116 114 111 108 108 032 117 032 124 032 099 097 107 101 032 105 115 032 097 032 108 105 101'
 
 button.MouseButton1Click:Connect(loadstring(game:HttpGet'https://raw.githubusercontent.com/MrbaconXD/GUI/main/BypassAntiCheatMessage.lua'), true)
 
-wait(3)

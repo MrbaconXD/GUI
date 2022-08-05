13e450292cf074601afb0f1268cd9941765daaa9
BaconXD <97074586+MrbaconXD@users.noreply.github.com>
2022-08-05 15:57:57 +0700

    Create BypassAntiCheatMessage.lua

---
+++ BypassAntiCheatMessage.lua
@@ -0,0 +1,12 @@
+--Fixing oof
+--local TextService = game:GetService("TextService")
+--local filteredTextResult = TextService:FilterStringAsync(text, fromPlayerId)
+
+wait(3)
+local b = BrickColor.new(“Bright green”)
+game.StarterGui:SetCore(“ChatMakeSystemMessage”, {
+Text = “[Server]: Access Successfully to trolling a hacking user”;
+Font = Arial;
+Color = b.Color;
+FontSize = Arial.FontSize.Size96;
+})

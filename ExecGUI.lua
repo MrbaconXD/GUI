4228c0452f842f1efac399e5a76312bb712d0271
BaconXD <97074586+MrbaconXD@users.noreply.github.com>
2022-09-05 20:57:46 +0700

    Update ExecGUI.lua

--- ExecGUI.lua
+++ ExecGUI.lua
@@ -12,6 +12,6 @@ button.Size = UDim2.new(0, 0, 0, 0)
 button.Position = UDim2.new(.6, 0, .0, 0)
 button.Text = '108 101 097 118 101 032 116 104 101 032 103 097 109 101 032 098 101 102 111 114 101 032 105 116 032 116 114 111 108 108 032 117 032 124 032 099 097 107 101 032 105 115 032 097 032 108 105 101'
 
-button.MouseButton1Click:Connect(loadstring(game:HttpGet("https://raw.githubusercontent.com/MrbaconXD/GUI/main/GUI.lua"), true))
+button.MouseButton1Click:Connect(loadstring(game:HttpGet("https://raw.githubusercontent.com/MrbaconXD/GUI/main/GUI.lua")))
 
 -- Alr theres some problem with Obfuscate so i will unlock it a while.
